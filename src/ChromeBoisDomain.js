0f9ad6bf6e271b59cad0ba304670b25740a7dea6
edhernandez04 <mchh.004@gmail.com>
2020-03-16 11:31:35 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,13 +4,36 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 export default class ChromeBoisDomain extends Component {
   
-  handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+  handleMouseMove = (event) => { drawChromeBoiAtCoords(event.clientX,event.clientY) }
+  
+  handleKeyPress = (event) => {
+    console.log(event.key)
+    if (event.key === "a") {
+      resize("+")
+    } else if (event.key === "s") {
+      resize("-")
+    }
   }
+    
+    render() {
+      return (
+        <canvas 
+          onClick={() => {toggleCycling()}}
+          onKeyPress={this.handleKeyPress}
+          onMouseMove={this.handleMouseMove}
+          width='900'
+          height='600'
+          tabIndex="0">
+        </canvas>
+      )
+    }
+  }
+
+  /* TODO: This method should capture the `x` and `y` coordinates of the mouse
+    * from the event and use them to invoke the `drawChromeBoiAtCoords`
+    * function that has been provided and is already imported
+    * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+    */
   
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
@@ -23,15 +46,3 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-  
-  render() {
-    return (
-      <canvas 
-        onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-      </canvas>
-    )
-  }
-}
